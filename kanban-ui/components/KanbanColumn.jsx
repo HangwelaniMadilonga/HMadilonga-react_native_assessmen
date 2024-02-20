700ca8d7ece5b307dae05cb93297647940e079c5
Hangwelani Madilonga <hangwelani3@gmail.com>
2024-02-20 14:35:08 +0200

    Change width of kanban board.

--- kanban-ui/components/KanbanColumn.jsx
+++ kanban-ui/components/KanbanColumn.jsx
@@ -1,14 +1,16 @@
 import { Container } from 'postcss';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, TextInput, StyleSheet} from 'react-native';
 
 const KanbanColums = () => {
     return (
       <View style={styles.mainContainer}>
-        <View style={styles.cardHeader}>
+        <View style={styles.columnHeader}>
             <Text style={styles.normalText}>ToDo</Text>
         </View>
         <View style= {styles.cardBody}>
+            <TextInput style={styles.cardInfo}>
 
+            </TextInput>
         </View>
       </View>
     );
@@ -27,11 +29,11 @@ const KanbanColums = () => {
     fontWeight: 'bold',
     color: 'white',
   },
-  cardHeader: {
+  columnHeader: {
     marginTop: 80,
     
     backgroundColor: '#3b82f6',
-    width: 250,
+    width: 300,
     minHeight: 20,
     justifyContent: 'center',
     alignItems: 'center',
@@ -40,13 +42,24 @@ const KanbanColums = () => {
 
   },
   cardBody: {
-    
-    
+
     backgroundColor: 'rgba(59,130,246,0.21)',
-    width: 250,
-    minHeight: 90,
+    width: 300,
+    minHeight: 120,
+    flex: 1,
+    
     justifyContent: 'center',
     alignItems: 'center',
+
+  },
+  cardInfo: {
+
+    backgroundColor: 'white',
+    width: 280,
+    minHeight: 100,
+    justifyContent: 'center',
+    alignItems: 'center',
+    
 
   },
 
