c75379fcec22284756704b964f8ec71f61302e32
Hangwelani Madilonga <hangwelani3@gmail.com>
2024-02-22 01:23:38 +0200

    Make Board screen invisible when adding tasks.

--- kanban-ui/App.js
+++ kanban-ui/App.js
@@ -2,16 +2,29 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, SafeAreaView, View, ScrollView } from "react-native";
 import BoardScreen from "./screens/BoardScreen";
 import AddButton from "./components/AddButton";
-import { ModalProvider } from "./components/ModalContext";
+import { ModalProvider, useModalContext } from "./components/ModalContext";
+
+const Content = () => {
+  //Global state from ModalContextt
+  const [modalVisible, setModalVisible] = useModalContext();
+
+  if (modalVisible) {
+    return null;
+  } else {
+    return (
+      <ScrollView>
+        <BoardScreen />
+      </ScrollView>
+    );
+  }
+};
 
 export default function App() {
   return (
     <ModalProvider>
       <View style={styles.container}>
         <AddButton />
-        <ScrollView>
-          <BoardScreen />
-        </ScrollView>
+        <Content />
       </View>
     </ModalProvider>
   );
